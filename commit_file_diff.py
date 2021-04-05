f9939e88460d9449bf10efe5fa0476ca25ea1afc
Keisuke Yamaguchi <phicdy@gmail.com>
2021-04-05 20:12:22 +0900

    Split output

--- commit_file_diff.py
+++ commit_file_diff.py
@@ -2,4 +2,5 @@ import subprocess
 from subprocess import PIPE
 
 proc = subprocess.run('git log -3 --pretty=format:"%h %s"', shell=True,  stdout=PIPE, stderr=PIPE, text=True)
-print(proc.stdout)
+print(proc.stdout.split())
+print(type(proc.stdout.split()))
