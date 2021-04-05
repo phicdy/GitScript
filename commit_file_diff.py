66c5edbce0dc3f5786eb47b1c9b8f35124666c6a
Keisuke Yamaguchi <phicdy@gmail.com>
2021-04-05 20:47:39 +0900

    Print index

--- commit_file_diff.py
+++ commit_file_diff.py
@@ -2,21 +2,20 @@ import subprocess
 from subprocess import PIPE
 
 proc = subprocess.run('git log -3 --pretty=format:"%h %s"', shell=True,  stdout=PIPE, stderr=PIPE, text=True)
-print(proc.stdout)
-
 lines = proc.stdout.split("\n")
+[print(str(i) + ") " + commit) for i, commit in enumerate(lines)]
 
 input_commit_index = input("\nEnter commit index\n")
 selected_commit = lines[int(input_commit_index)].split()[0]
 selected_previous_commit = lines[int(input_commit_index)+1].split()[0]
 
-proc_diff = subprocess.run('git diff ' + selected_commit + ' --name-only', shell=True,  stdout=PIPE, stderr=PIPE, text=True)
+proc_diff = subprocess.run('git diff ' + selected_previous_commit + '..' + selected_commit + ' --name-only', shell=True,  stdout=PIPE, stderr=PIPE, text=True)
+line_files = proc_diff.stdout.strip().split("\n")
+
 print("\nFiles:")
-print(proc_diff.stdout)
+[print(str(i) + ") " + file) for i, file in enumerate(line_files)]
 
-line_files = proc_diff.stdout.split("\n")
-
-input_file = input("Enter file index\n")
+input_file = input("\nEnter file index\n")
 selected_file = line_files[int(input_file)].split()[0]
 print(selected_file)
 
